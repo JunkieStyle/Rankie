5711cb24a51ad082e18907f5e9928eeb76dc5a61
Anton Nekhaev <antonnekhaev@gmail.com>
2022-07-19 20:19:54 +0300

    added run_dir and fixed settings

--- geekathlon/settings.py
+++ geekathlon/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+RUN_DIR = BASE_DIR / "run"
 
 
 # Quick-start development settings - unsuitable for production
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'geekathlon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': RUN_DIR / 'db.sqlite3',
     }
 }
 
