426443126f17e307b986aa7bdac56c0b5c3d2c0a
Anton Nekhaev <antonnekhaev@gmail.com>
2022-07-20 10:57:18 +0300

    renamed project to 'rankie'

--- rankie/asgi.py
+++ rankie/asgi.py
@@ -1,4 +1,4 @@
-"""ASGI config for geekathlon project.
+"""ASGI config for rankie project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -10,6 +10,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geekathlon.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rankie.settings")
 
 application = get_asgi_application()
--- rankie/urls.py
+++ rankie/urls.py
@@ -1,4 +1,4 @@
-"""geekathlon URL Configuration.
+"""rankie URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
