9f2ee72d53a4d1ae6430c1d1164c4c539708f82d
Anton Nekhaev <antonnekhaev@gmail.com>
2022-07-21 16:20:22 +0300

    fixed static files serving

--- rankie/settings/prod.py
+++ rankie/settings/prod.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # Simplified static file serving
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -130,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = RUN_DIR / "static"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
