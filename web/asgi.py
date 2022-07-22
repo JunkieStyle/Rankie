e488c8947d36dca6beac365f86723c65ac4efc29
Anton Nekhaev <antonnekhaev@gmail.com>
2022-07-22 19:02:59 +0300

    refactored web

--- web/asgi.py
+++ web/asgi.py
@@ -10,6 +10,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rankie.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 
 application = get_asgi_application()
