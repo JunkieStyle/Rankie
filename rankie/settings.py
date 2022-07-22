02abed06c24b767a162778a8e9d16287f68d43e4
Anton Nekhaev <antonnekhaev@gmail.com>
2022-07-22 16:09:41 +0300

    debugging telegram

--- rankie/settings.py
+++ rankie/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # "django.middleware.security.SecurityMiddleware",
+    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",  # Simplified static file serving
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
