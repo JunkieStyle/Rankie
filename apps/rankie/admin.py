f01e8f8bfe2e20862bdbf4606a2a46024e6af615
Anton Nekhaev <antonnekhaev@gmail.com>
2022-07-25 20:13:12 +0300

    fixed admin classes

--- apps/rankie/admin.py
+++ apps/rankie/admin.py
@@ -6,54 +6,45 @@ from .models import Game, Round, League, GameRule, Standing, GameResult, RoundRe
 
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
-    list_display_links = ["label"]
-    list_display = ("label", "name", "url", "created", "updated")
+    list_display = ("id", "label", "name", "url", "created", "updated")
+    list_display_links = ("id", "label", "name")
     date_hierarchy = "created"
 
 
 @admin.register(GameResult)
 class GameResultAdmin(admin.ModelAdmin):
-    list_display = ("get_game_label", "get_game_round", "get_username", "created")
+    list_display = ("id", "get_game_label", "player", "origin", "created")
     list_select_related = ("player", "game")
     date_hierarchy = "created"
-
-    @display(ordering="player__username", description="Player")
-    def get_username(self, obj):
-        return obj.player.username
 
     @display(ordering="game__label", description="Game")
     def get_game_label(self, obj):
         return obj.game.label
 
-    @display(ordering="game__round", description="Round")
-    def get_game_round(self, obj):
-        return obj.game.round
-
 
 @admin.register(GameRule)
 class GameRuleAdmin(admin.ModelAdmin):
-    list_display = ("game", "name", "created", "updated")
+    list_display = ("id", "game", "name", "created", "updated")
     list_select_related = ["game"]
 
 
 @admin.register(League)
 class LeagueAdmin(admin.ModelAdmin):
-    list_display_links = ("label", "rule")
-    list_display = ("label", "name", "rule", "owner", "start_dt", "end_dt", "created", "updated")
+    list_display_links = ("id", "label")
+    list_display = ("id", "label", "name", "rule", "owner", "start_dt", "end_dt", "created", "updated")
     list_select_related = ["owner"]
 
 
 @admin.register(Round)
 class Round(admin.ModelAdmin):
-    list_display_links = ("label", "league")
-    list_display = ("label", "league", "mvp")
+    list_display_links = ("id", "label")
+    list_display = ("id", "label", "league", "mvp")
     list_select_related = ("league", "mvp")
 
 
 @admin.register(RoundResult)
 class RoundResult(admin.ModelAdmin):
-    list_display_links = ["raw"]
-    list_display = ("get_round_league", "get_round_label", "player", "score", "raw", "created", "updated")
+    list_display = ("id", "get_round_league", "get_round_label", "player", "score", "raw", "created", "updated")
     list_select_related = ("round__league", "player", "raw")
 
     @display(ordering="round__label", description="Round")
@@ -67,6 +58,5 @@ class RoundResult(admin.ModelAdmin):
 
 @admin.register(Standing)
 class Standing(admin.ModelAdmin):
-    list_display_links = ["league"]
-    list_display = ("league", "rank", "player", "score", "mvp_count", "created", "updated")
+    list_display = ("id", "league", "rank", "player", "score", "mvp_count", "created", "updated")
     list_select_related = ("league", "player")
