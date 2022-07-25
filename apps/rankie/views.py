ed4e9e22075d8a81227a454b1a39fbefbd5e2b2c
Anton Nekhaev <antonnekhaev@gmail.com>
2022-07-25 20:56:51 +0300

    fixed register view response

--- apps/rankie/views.py
+++ apps/rankie/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 from rest_framework.decorators import action
 
 from .core import register_game_result
@@ -14,3 +15,4 @@ class GameResultViewSet(viewsets.ModelViewSet):
     def register(self, request, *args, **kwargs):
         game_result = self.get_object()
         register_game_result(game_result)
+        return Response(status=status.HTTP_200_OK)
